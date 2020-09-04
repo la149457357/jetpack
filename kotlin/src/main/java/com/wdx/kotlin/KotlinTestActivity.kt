8a4984bc856de3a84bed5e5923a63b1d9165df73
wangdx01 <wangdx01@missfresh.cn>
2020-09-04 15:35:39 +0800

    测试凯特琳哒哒哒哒哒

---
+++ kotlin/src/main/java/com/wdx/kotlin/KotlinTestActivity.kt
@@ -0,0 +1,8 @@
+package com.wdx.kotlin
+
+/**
+ *
+ * @ Description:
+ * @ Author: wdx
+ * @ CreateDate: 2020/9/4 11:32
+ */
---
+++ kotlin/src/main/java/com/wdx/kotlin/TestKotlinDemo.kt
@@ -0,0 +1,8 @@
+package com.wdx.kotlin
+
+/**
+ *
+ * @ Description:
+ * @ Author: wdx
+ * @ CreateDate: 2020/9/4 14:16
+ */
